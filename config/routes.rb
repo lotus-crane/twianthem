9611bb79235383ca8d34e1886f7e33d8a9b6ae9b
Ryo Tsuruhashi <s1711543@s.tsukuba.ac.jp>
2019-06-19 21:08:49 +0900

    route.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/show'
+  get 'pages/timeline'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
 end
