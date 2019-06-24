1c2bccf422e4067f6498a4bca4a5afa6d03af1fe
Ryo Tsuruhashi <s1711543@s.tsukuba.ac.jp>
2019-06-25 00:40:37 +0900

    終わり、見えてきたね（TRUEENDぶり）

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   before_action :login_required, only: [:timeline, :tweet]
+  helper_method :timeline, :test_bro
   include Common
   require 'twitter'
 
@@ -15,15 +16,21 @@ class PagesController < ApplicationController
     #@tweets = client.home_timeline(include_entities: true)
   #end
 
-  #def timeline_page
-  #  @play_library = []
-  #  client = client_new
-  #  client.home_timeline.each do |tweet|
-    #  if tweet include("#nowplaying")
-      #  @play_library.push("\e[33m" + tweet.user.name + "\e[32m" + "[ID:" + tweet.user.screen_name + "]\e[0m" + tweet.text)
-    #  end
-  #  end
-  #end
+  def timeline
+   play_library = []
+    client = client_new
+    client.home_timeline.each do |tweet|
+      #if tweet include("#nowplaying")
+        play_library.push("#{tweet.user.name} ID:@#{tweet.user.screen_name},  #{tweet.text}")
+      #end
+    end
+    return play_library
+  end
+# max_idを引数にしてそれより過去のをさかのぼっていくのは＿
+
+  def test_bro
+
+  end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get 'top/timeline'
   root 'pages#index'
   get 'pages/show'
-  get '/pages/timeline_page', to: 'pages#timeline_page'
-  get '/pages/nowplaying', to: 'pages#timeline_page'
+  get 'pages/timeline_page'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
 end
